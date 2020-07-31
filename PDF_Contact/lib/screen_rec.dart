0c5eb0c4098c136db11a92abcbea9cd2443284ac
Prat15072000 <pratsimple2000@gmail.com>
2020-07-31 21:31:42 +0530

    Create screen_rec.dart
    Trying to implement the screen recording functionality

---
+++ PDF_Contact/lib/screen_rec.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Rec extends StatefulWidget {
+  @override
+  _RecState createState() => _RecState();
+}
+
+class _RecState extends State<Rec> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
